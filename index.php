2b20cd142b1dd2c0e04560e889411950c3a8550a
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-29 15:31:12 +0100

    done!

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 class Car {
 
+    public $brand = 'Toyota';
+    public $model = 'Prius';
+    public $year = 2010;
+
+    public function start()
+    {
+        // return "$this->year said Vroom! Vroom!!";
+        echo $this->year . " said Vroom! Vroom";
+    }
+
 }
 
 new Car();
@@ -9,8 +19,28 @@ $carOne = new Car();
 
 $carTwo = new Car();
 
-get_class($carOne);
-echo "I love ". get_class($carTwo)
+echo $carOne->brand . '<br>';
+echo $carOne->model . '<br>';
+echo $carTwo->start() . '<br>';
+
+echo '<hr>';
+$carTwo->brand = 'Honda';
+$carTwo->model = 'Civic';
+$carTwo->year = '1999';
+
+echo $carTwo->brand . '<br>';
+echo $carTwo->model . '<br>';
+echo $carTwo->start() . '<br>';
+
+
+
+// get_class($carOne);
+// echo "I love " . get_class($carTwo);
+
+
+// print_r(get_class_methods(get_class($carOne)));
+
+// print_r( get_class_vars('Car'));
 
 ?>
 
