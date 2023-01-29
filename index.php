a37c5541e2ab28c8bbf2db353b011a96a720eaac
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-29 14:25:33 +0100

    start oop netninja

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
-require 'functions.php';
-require 'router.php';
+echo 'hello, ninjas';
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <title>PHP OOP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
